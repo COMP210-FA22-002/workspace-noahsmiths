49e6b2e7b81bc5d9370c604839e7a7e890f86066
noahsmiths <nsmith2@unc.edu>
2022-10-18 02:07:39 +0000

    assignment 4 done (that was a little tough not gonna lie)

--- assignments/src/assn04/Main.java
+++ assignments/src/assn04/Main.java
@@ -1,20 +1,37 @@
 package assn04;
 
-
 public class Main {
-  public static void main(String[] args) {
-    /*
-    This is a basic example of how to create a BST and add values to it.
-    You should add more examples and use this class to debug your code
-    */
-    BST<Integer> bst = new NonEmptyBST<Integer>(3);
-      bst = bst.insert(8);
-      bst = bst.insert(1);
-      bst = bst.insert(9);
-      bst = bst.insert(4);
+    public static void main(String[] args) {
+        /*
+         * This is a basic example of how to create a BST and add values to it.
+         * You should add more examples and use this class to debug your code
+         */
+        // BST<Integer> bst = new NonEmptyBST<Integer>(3);
+        // bst = bst.insert(8);
+        // bst = bst.insert(1);
+        // bst = bst.insert(9);
+        // bst = bst.insert(4);
+        BST<Integer> bst = new NonEmptyBST<Integer>(78);
+        bst.insert(31);
+        bst.insert(84);
+        bst.insert(13);
+        bst.insert(38);
+        bst.insert(10);
+        bst.insert(14);
+        bst.insert(40);
+        bst.insert(12);
+        bst.insert(39);
+        bst.insert(47);
 
+        // bst.insert(32);
+        
+        bst.printBreadthFirstTraversal();
 
+        System.out.println("");
 
-  }
+        bst.remove(31);
+
+        bst.printBreadthFirstTraversal();
+    }
 
 }
--- assignments/src/assn04/NonEmptyBST.java
+++ assignments/src/assn04/NonEmptyBST.java
@@ -19,35 +19,115 @@ public class NonEmptyBST<T extends Comparable<T>> implements BST<T> {
 	// TODO: insert
 	@Override
 	public BST<T> insert(T element) {
+		switch (element.compareTo(this.getElement())) {
+			case 0: // Number is equal to current
+				// return this;
+				break;
+			case -1: // Number is less than current
+				if (this.getLeft().isEmpty()) {
+					this._left = new NonEmptyBST<T>(element);
+				} else {
+					this.getLeft().insert(element);
+				}
+				break;
+				// return this;
+			case 1: // Number is greater than current
+				if (this.getRight().isEmpty()) {
+					this._right = new NonEmptyBST<T>(element);
+				} else {
+					this.getRight().insert(element);
+				}
+				break;
+		}
 
-		
+		return this;
 	}
 
 	// TODO: remove
 	@Override
 	public BST<T> remove(T element) {
+		switch (element.compareTo(this.getElement())) {
+			case 0: // Number is equal to current
+				if (this.getLeft().isEmpty() && this.getRight().isEmpty()) { // No children
+					return new EmptyBST<T>();
+				}
 
+				if (this.getLeft().isEmpty()) { // Only right child
+					return this.getRight();
+				} else if (this.getRight().isEmpty()) { // Only left child
+					return this.getLeft();
+				} else { // Both children exist
+					NonEmptyBST<T> currentNode = (NonEmptyBST<T>) this.getRight();
 
+					while (!currentNode.getLeft().isEmpty()) { // Get leftmost child of right side, aka closest larger value to the one being removed
+						currentNode = (NonEmptyBST<T>) currentNode.getLeft();
+					}
+
+					T temp = currentNode.getElement(); // Store the value
+					this.remove(currentNode.getElement()); // Remove it from wherever it was
+					this._element = temp; // Assign the value to the root of the subtree
+
+					return this;
+				}
+			case -1: // Number is less than current
+				this._left = this.getLeft().remove(element);
+				break;
+			case 1: // Number is greater than current
+				this._right = this.getRight().remove(element);
+				break;
+		}
+
+		return this;
 	}
+
 
 	// TODO: printPreOrderTraversal
 	@Override
 	public void printPreOrderTraversal() {
-	
-	
+		System.out.print(this.getElement() + " ");
+		
+		if (!this.getLeft().isEmpty()) {
+			this.getLeft().printPreOrderTraversal();
+		}
+
+		if (!this.getRight().isEmpty()) {
+			this.getRight().printPreOrderTraversal();
+		}
 	}
 
 	// TODO: printPostOrderTraversal
 	@Override
 	public void printPostOrderTraversal() {
+		if (!this.getLeft().isEmpty()) {
+			this.getLeft().printPostOrderTraversal();
+		}
+
+		if (!this.getRight().isEmpty()) {
+			this.getRight().printPostOrderTraversal();
+		}
 	
-	
+		System.out.print(this.getElement() + " ");
 	}
 
 	// TODO: printBreadthFirstTraversal
 	@Override
 	public void printBreadthFirstTraversal() {
-	
+		LinkedList queue = new LinkedList<BST<T>>();
+		queue.add(this);
+
+		while (!queue.isEmpty()) {
+			BST<T> tree = (BST<T>) queue.poll();
+
+			System.out.print(tree.getElement() + " ");
+
+			if (!tree.getLeft().isEmpty()) {
+				queue.add(tree.getLeft());
+			}
+
+			if (!tree.getRight().isEmpty()) {
+				queue.add(tree.getRight());
+			}
+		}
 	
 	}
 
